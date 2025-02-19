38d3c7312c6ecf17751cf7e2b1d929cc9ff857c8
u-sernam-e <carstens.jack.liam@gmail.com>
2025-02-18 21:59:55 -0500

    two players

--- player.gd
+++ player.gd
@@ -6,6 +6,7 @@ const ACCELERATION = 100.0
 const JUMP_VELOCITY = -600.0
 const FAST_FALL_VELOCITY = 1000.0
 
+@export var player_number = 1
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,11 +14,12 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("p1_up") and is_on_floor():
+	if Input.is_action_just_pressed(player_input("up", player_number)) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		$JumpSound.play()
 	
 	#fast falling
-	if Input.is_action_pressed("p1_down") and !is_on_floor():
+	if Input.is_action_pressed(player_input("down", player_number)) and !is_on_floor():
 		if velocity.y > 0:
 			velocity.y = FAST_FALL_VELOCITY # if falling, set a high velocity down
 		else:
@@ -25,11 +27,22 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("p1_left", "p1_right")
-	print(direction)
+	var direction := Input.get_axis(player_input("left", player_number), player_input("right", player_number))
 	if direction:
 		velocity.x = move_toward(velocity.x, MAX_SPEED * direction, ACCELERATION)
 	else:
 		velocity.x = move_toward(velocity.x, 0, ACCELERATION)
 
 	move_and_slide()
+
+
+func _on_death_plane_body_entered(body: Node2D) -> void:
+	print(str(player_number) + ", " + str(body.player_number))
+	if body.player_number == player_number:
+		die()
+
+func die() -> void:
+	$DeathSound.play()
+
+func player_input(input : String, p_number : int) -> String:
+	return "p" + str(p_number) + "_" + input
